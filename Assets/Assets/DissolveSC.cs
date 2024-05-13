95f72674b9e7c14d53173ce27f303f5b1c2cfbbf
Goose <107567328+SomeRandomGoose@users.noreply.github.com>
2024-05-13 17:50:50 +0200

    Pushhh

--- Assets/Assets/DissolveSC.cs
+++ Assets/Assets/DissolveSC.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DissolveSC : MonoBehaviour
 {
+    public bool can_dissolve;
+    public float dissolveSpeed;
+    public float initDiss = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,55 @@ public class DissolveSC : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hitInfo))
+            {
+                if (hitInfo.collider.gameObject == this.gameObject)
+                {
+                    if (!can_dissolve)
+                        gameObject.GetComponent<AudioSource>().Play();
+
+                    can_dissolve = true;
+             
+                }
+              
+
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        DissolveSofa();
+    }
+    void DissolveSofa()
+    {
+        if(can_dissolve)
+        {
+            print("Oh Yeah~");
+            GetComponent<MeshRenderer>().material.SetFloat("DT", initDiss);
+            initDiss += 1 * dissolveSpeed * Time.deltaTime;
+
+
+            if(initDiss > 1)
+            {
+                //gameObject.GetComponent<AudioSource>().Stop();
+                initDiss = 0;
+                GetComponent<MeshRenderer>().material.SetFloat("DT", 0);
+                can_dissolve = false;
+            }
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
+    }
+    private void OnMouseExit()
+    {
+        Color color = new Color32(241, 157, 157, 255);
+        GetComponent<MeshRenderer>().material.SetColor("_Color", color);
     }
 }
